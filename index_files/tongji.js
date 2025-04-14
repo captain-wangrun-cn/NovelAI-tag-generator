d4f2887caa3514c0a0360c7f42df12ab448924ce
userName <captain-wangrun-cn>
2025-04-14 23:10:40 +0800

    添加百度统计

---
+++ index_files/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?20e899ae6f8c9ee2ec9c79946a0774a5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
